c3e0bb1b55e1f04b57ad4dfcecf980154f2d7fe7
Sebastian Illa <seba.illa.99@gmail.com>
2022-09-21 21:37:27 -0300

    Creand carrito

--- js/main.js
+++ js/main.js
@@ -1,121 +1,102 @@
+var esmalte = {
+    tipo: 1,
+    rendimiento: 8,
+    precio: 100,
+};
+
 var cieloRaso = {
     tipo: 2,
     rendimiento: 10,
-    volumenChi: 1,
-    volumenMed: 4,
-    volumenGra: 20,
-    valorChi: 100,
-    valorMed: 300,
-    valorGra: 1700,
+    precio: 200,
 };
 
 var epoxi = {
     tipo: 3,
     rendimiento: 6,
-    volumenChi: 1,
-    volumenMed: 4,
-    volumenGra: 20,
-    valorChi: 100,
-    valorMed: 300,
-    valorGra: 1700,
+    precio: 300,
 };
 
+
+var rendimiento;
 var superficie;
 var tipo;
-var compra;
-var valor
-const carrito = [];
+var precio;
+var carrito = [];
 
-const esmalte = new latas("Esmalte", 1, 8, 1, 4, 20, 100, 300, 1700)
-
-function latas(nombre, tipo, rendimiento, volumenChi, volumenMed, volumenGra, valorChi, valorMed, valorGra) {
-    this.nombre = nombre
-    this.tipo = tipo
-    this.rendimiento = rendimiento
-    this.volumenChi = volumenChi
-    this.volumenMed = volumenMed
-    this.volumenGra = volumenGra
-    this.valorChi = valorChi
-    this.valorMed = valorMed
-    this.valorGra = valorGra
-};
 
 function cantLitros(superficie, rendimiento) {
     return superficie / rendimiento
 };
 
-    let flag = true
-    do {
-        tipo = prompt("Elige una opcion: \n1. Esmalte \n2. CieloRaso \n3. Epoxi \n4. Carrito \n5. Salir");
+do {
+    tipo = prompt("Elige una opcion: \n1. Esmalte \n2. CieloRaso \n3. Epoxi \n4. Carrito \n5. Seguir a la tienda");
 
-        if (tipo == 1) {
-            alert("El rendimiento por litro son 8 metros cuadrados")
-            rendimiento = esmalte.rendimiento
+    if (tipo == 1) {
+        alert("El rendimiento por litro son 8 metros cuadrados");
+        rendimiento = esmalte.rendimiento
+        superficie = prompt("Ingrese la cantidad de metros cuadrados que desea pintar")
+        alert("La cantidad de litros nescesaria seran: " + cantLitros(superficie, rendimiento) + " L")
 
-        } else if (tipo == 2) {
-            alert("El rendimiento por litro son 10 metros cuadrados")
-            rendimiento = cieloRaso.rendimiento
-
-
-        } else if (tipo == 3) {
-            alert("El rendimiento por litro son 6 metros cuadrados")
-            rendimiento = epoxi.rendimiento
-
-        } else if (tipo == 4) {
-            alert(` Tu Carrito :${carrito} `)
-
-        } else if (tipo == 5) {
-            flag = false
-        } else {
-            alert("La opcion no es valida")
-
-        }
-    } while (tipo < 1 || tipo > 3);
-
-    superficie = prompt("Ingrese la cantidad de metros cuadrados que desea pintar")
-    alert("La cantidad de litros nescesaria seran: " + cantLitros(superficie, rendimiento) + " L")
-    do {
-        compra = prompt("Desea añadir al carrito? Volumen: \n1. 1L \n2. 4L \n3. 20L");
-        if (tipo == 1, compra == 1) {
-            valor = esmalte.valorChi
-            volumenChi
-            carrito.push(`${esmalte.nombre} de ${esmalte.volumenChi} L de valor ${esmalte.valorChi}`)
-
-        } else if (tipo == 1, compra == 2) {
-            valor = esmalte.valorMed
-
-            carrito.push(esmalte.valorMed)
+    } else if (tipo == 2) {
+        alert("El rendimiento por litro son 10 metros cuadrados");
+        rendimiento = cieloRaso.rendimiento
+        superficie = prompt("Ingrese la cantidad de metros cuadrados que desea pintar")
+        alert("La cantidad de litros nescesaria seran: " + cantLitros(superficie, rendimiento) + " L")
 
 
-        } else if (tipo == 1, compra == 3) {
-            valor = esmalte.valorGra
+    } else if (tipo == 3) {
+        alert("El rendimiento por litro son 6 metros cuadrados");
+        rendimiento = epoxi.rendimiento
+        superficie = prompt("Ingrese la cantidad de metros cuadrados que desea pintar")
+        alert("La cantidad de litros nescesaria seran: " + cantLitros(superficie, rendimiento) + " L")
+
+    } else if (tipo == 4) {
+        alert(` Tu Carrito :${carrito} `);
+
+    } else if (tipo == 5) {
+        break;
+    } else {
+        alert("La opcion no es valida");
+    }
+}
+while (tipo != 5);
 
 
+class Producto {
+    constructor(nombre, cantidad) {
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+    }
+}
 
+do {
+    var comprobacion = prompt("Ingrese el nombre de la pintura o fin para terminar de agregar")
+    if (comprobacion === "Fin" || comprobacion === "FIN" || comprobacion === "fin") {
+        break;
+    } else {
+        nombreP = comprobacion;
+        var cantidadP = prompt("Ingrese la cantidad que desea comprar");
+        carrito.push(new Producto(nombreP, cantidadP))
+    }
+}
+while (comprobacion != "Fin" || comprobacion != "FIN" || comprobacion != "fin")
 
-        } else if (tipo == 2, compra == 1) {
+console.log(carrito)
 
+for (var producto of carrito) {
+    let contenedor = document.createElement("div");
+    contenedor.innerHTML = `<h3> Nombre: ${producto.nombre}</h3>
+                            <p>Cantidad: ${producto.cantidad}</p>`
+    document.body.appendChild(contenedor)
+}
 
-        } else if (tipo == 2, compra == 2) {
+var ingresado = prompt("Buscar...");
+var prodIngresado = carrito.filter(producto => producto.nombre.includes(ingresado))
+console.log(prodIngresado);
+document.write("<h3> Lista de productos relacionados con su busqueda")
 
-
-        } else if (tipo == 2, compra == 3) {
-
-
-
-
-
-        } else if (tipo == 3, compra == 1) {
-
-
-        } else if (tipo == 3, compra == 2) {
-
-
-        } else if (tipo == 3, compra == 3) {
-
-
-        } else {
-
-        }
-    } while (compra < 1 || compra > 3);
-    while (flag);
+for (var producto of prodIngresado) {
+    let contenedor = document.createElement("div");
+    contenedor.innerHTML = `<h3> Nombre: ${producto.nombre}</h3>`
+    document.body.appendChild(contenedor)
+}
